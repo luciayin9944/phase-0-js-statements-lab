563fdeb5104ac82c87880b2091ba6646df40a3f0
luciayin9944 <luxiayin94@gmail.com>
2025-04-09 14:23:14 -0700

    updated gardenManagement.js

--- gardenManagement.js
+++ gardenManagement.js
@@ -1,3 +1,23 @@
 const temperature = 90;
 const timeOfDay = "morning";
 let soilMoisture = 30;
+
+// if-else statement
+if (temperature > 80){
+    console.log("Watering on");
+} else {
+    console.log("Watering off");
+}
+
+// if-else statement
+if (timeOfDay=== "evening" | timeOfDay==="night"){
+    console.log("Lights on");
+} else {
+    console.log("Lights off")
+}
+
+// while loop
+while (soilMoisture<=40){
+    console.log(soilMoisture);
+    soilMoisture = soilMoisture + 5;
+}
